efcbfb94248ed99148aa14350662a9b11181a320
Luke Horwell <code@horwell.me>
2021-03-21 21:14:57 +0000

    Add script to watch locally

---
+++ scripts/watch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Previews the Jekyll-powered website locally.
+#
+# On Arch, install "ruby-bundler" and run "bundle install" in this repository.
+#
+bundle exec jekyll serve --watch
